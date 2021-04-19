5f32036d5478a67aa282b07e4a6e39704bea7822
halfbird283 <82825882+halfbird283@users.noreply.github.com>
2021-04-19 18:46:02 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+print('Hello my name is gini...')
+print('Give me a number, I can magicly determine whether its even or odd.')
+print('It can be any number you can think of...')
+print('anything!')
+print('Ready? Set.')
+print('GO!')
+x = int(input('Enter any number here'))
+r = x % 2
+if (r==0):
+    print(x,'is an even number')
+else:
+    print(x, 'is an odd number')
+
+print('Magic!')
